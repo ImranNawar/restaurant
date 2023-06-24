5a514131ccea6c3caabed8bf71e893efa8618676
ImranNawar <imran1nawar@gmail.com>
2023-06-24 12:55:46 +0500

    remove

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import HomePage from './routes/HomePage'
-import BookingPage from './routes/BookingPage';
-import About from './routes/About';
-import Order from './routes/Order';
-import Reservations from './routes/Reservations';
-import Login from './routes/Login';
-import Menu from './routes/Menu';
-import Error from './routes/Error'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
+import HomePage from "./components/pages/HomePage";
+import BookingPage from "./components/pages/BookingPage";
+
+function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/booking" element={<BookingPage />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/menu" element={<Menu />}></Route>
-          <Route path="/reservations" element={<Reservations />}></Route>
-          <Route path="/order" element={<Order />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/booking" element={<BookingPage />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
