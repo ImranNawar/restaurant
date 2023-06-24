66cac14c2c7dc2efff6966afb95da68103aec9e0
ImranNawar <imran1nawar@gmail.com>
2023-06-24 12:55:24 +0500

    Pages

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,21 @@
+import Header from "../Header";
+import CallToAction from "../CallToAction";
+import Specials from "../Specials"; 
+import CustomersSay from "../CustomersSay";
+import Footer from "../Footer";
+import Peshawar from "../Peshawar";
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <CallToAction />
+      <Specials />
+      <CustomersSay />
+      <Peshawar />
+      <Footer />
+    </>
+  );
+};
+
+export default Homepage;
